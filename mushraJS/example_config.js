f82fe24a6e368a379b16c0eda2d387bcf5180692
Joao Felipe Santos <joao.eel@gmail.com>
2015-01-18 19:37:28 -0500

    Changed files to the real ones

--- mushraJS/example_config.js
+++ mushraJS/example_config.js
@@ -1,6 +1,6 @@
 // configure the test here
 var TestData = {
-  "TestName": "Your Test Name",
+  "TestName": "MUSHRA Adaptive and Bandwidth Extension Subjective Tests",
   "RateScalePng": "img/scale_abs.png",
   "RateScaleBgPng": "img/scale_abs_background.png",
   "RateMinValue": 0,
@@ -10,30 +10,109 @@ var TestData = {
   "ShowFileIDs": false,
   "EnableABLoop": true,
   "EnableOnlineSubmission": true,
-  "SubmitResultsURL": "https://gentle-tundra-1517.herokuapp.com/submit",
-  "SupervisorContact": "jfsantos@emt.inrs.ca",
+  "SubmitResultsURL": "/submit",
+  "SupervisorContact": "rachel.bou.serhal@etsmtl.ca",
   "Testsets": [
-    //    
     {
       "Name": "Test 1",
-      "Reference": "test1/reference.wav",
+      "Reference": "test1/l1s1_ref.wav",
       "Files": [
-            "test1/1.wav",
-            "test1/2.wav",
-            "test1/3.wav",
-            "test1/4.wav",
+            "test1/l1s1_iem.wav",
+            "test1/l1s1_iem_NS.wav",
+            "test1/l1s1_iem_N.wav",
+            "test1/l1s1_iem_BWE.wav",
         ]
     },
-    //    
     {
-      "Name": "Not actually a test, all files are the same ;)",
-      "Reference": "test2/reference.wav",
+     "Name": "Test 2",
+      "Reference": "test2/l2s2_ref.wav",
       "Files": [
-            "test2/1.wav",
-            "test2/2.wav",
-            "test2/3.wav",
-            "test2/4.wav",
+            "test2/l2s2_iem.wav",
+            "test2/l2s2_iem_NS.wav",
+            "test2/l2s2_iem_N.wav",
+            "test2/l2s2_iem_BWE.wav",
         ]
     },
+    {
+      "Name": "Test 3",
+      "Reference": "test3/l3s3_ref.wav",
+      "Files": [
+            "test3/l3s3_iem.wav",
+            "test3/l3s3_iem_NS.wav",
+            "test3/l3s3_iem_N.wav",
+            "test3/l3s3_iem_BWE.wav",
+        ]
+    },
+    {
+      "Name": "Test 4",
+      "Reference": "test1/l4s4_ref.wav",
+      "Files": [
+            "test1/l4s4_iem.wav",
+            "test1/l4s4_iem_NS.wav",
+            "test1/l4s4_iem_N.wav",
+            "test1/l4s4_iem_BWE.wav",
+        ]
+    },
+    {
+     "Name": "Test 5",
+      "Reference": "test3/l5s5_ref.wav",
+      "Files": [
+            "test3/l5s5_iem.wav",
+            "test3/l5s5_iem_NS.wav",
+            "test3/l5s5_iem_N.wav",
+            "test3/l5s5_iem_BWE.wav",
+        ]
+    },
+    {
+      "Name": "Test 6",
+      "Reference": "test3/l5s6_ref.wav",
+      "Files": [
+            "test3/l5s6_iem.wav",
+            "test3/l5s6_iem_NS.wav",
+            "test3/l5s6_iem_N.wav",
+            "test3/l5s6_iem_BWE.wav",
+        ]
+    },
+    {
+      "Name": "Test 7",
+      "Reference": "test3/l6s6_ref.wav",
+      "Files": [
+            "test3/l6s6_iem.wav",
+            "test3/l6s6_iem_NS.wav",
+            "test3/l6s6_iem_N.wav",
+            "test3/l6s6_iem_BWE.wav",
+        ]
+    },
+    {
+     "Name": "Test 8",
+      "Reference": "test3/l8s1_ref.wav",
+      "Files": [
+            "test3/l8s1_iem.wav",
+            "test3/l8s1_iem_NS.wav",
+            "test3/l8s1_iem_N.wav",
+            "test3/l8s1_iem_BWE.wav",
+        ]
+    },
+    {
+      "Name": "Test 9",
+      "Reference": "test3/l9s3_ref.wav",
+      "Files": [
+            "test3/l9s3_iem.wav",
+            "test3/l9s3_iem_NS.wav",
+            "test3/l9s3_iem_N.wav",
+            "test3/l9s3_iem_BWE.wav",
+        ]
+    },
+    {
+      "Name": "Test 10",
+      "Reference": "test3/l3s6_ref.wav",
+      "Files": [
+            "test3/l3s6_iem.wav",
+            "test3/l3s6_iem_NS.wav",
+            "test3/l3s6_iem_N.wav",
+            "test3/l3s6_iem_BWE.wav",
+        ]
+    },
+
   ]
 }
